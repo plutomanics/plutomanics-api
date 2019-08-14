7e5514dbed2ea6fbac7bf26e245e6b3b6210f317
karanpopat <karanpopat@gmail.com>
2019-08-14 13:18:01 +0530

    first commit
    first commit

---
+++ src/main/java/com/project/plutomanicss/PlutomanicsApplication.java
@@ -0,0 +1,13 @@
+package com.project.plutomanicss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlutomanicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlutomanicsApplication.class, args);
+	}
+
+}
